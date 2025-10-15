eb10110f8c0382f2adbd90b53cac2e9f01bd8b05
Mohd Mujahed <mujahedu984@gmail.com>
2025-10-16 00:56:00 +0530

    update: src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025 XUNOIA TECHNOLOGIES PRIVATE LIMITED
+ * Licensed under XUNOIA Private License v1.0
+ * All rights reserved.
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
